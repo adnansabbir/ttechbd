f76dfa33ec9478add4312ff83a941b3975c7c3e6
adnansabbir <adnan.sabbir2012@hmail.com>
2019-08-30 02:56:56 +0600

    Added Project list

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 
+admin.site.register(models.ProjectPage)
 admin.site.register(models.Project)
 admin.site.register(models.ProjectImage)
 admin.site.register(models.Link)
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Project
+from .models import ProjectPage, Project
 
 
 def project_list(request):
-    return render(request, 'projects/project_list.html')
+    data = {
+        'project_page': ProjectPage.objects.first(),
+        'projects': Project.objects.all()
+    }
+    return render(request, 'projects/project_list.html', data)
