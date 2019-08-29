6782b7e344fefe424eae19d942544900c363948e
adnansabbir <adnan.sabbir2012@hmail.com>
2019-08-30 02:28:06 +0600

    Added project list

--- ttechbd/urls.py
+++ ttechbd/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('apps.homepage.urls')),
     path('aboutus/', include('apps.about_us.urls')),
+    path('projects/', include('apps.projects.urls')),
     path('admin/', admin.site.urls),
 ]
 
