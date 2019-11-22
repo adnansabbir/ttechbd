e05db8bc2f654d3b652eae7b32f595775bec53e2
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-23 02:21:09 +0600

    Affiliate projects added

--- apps/affiliations/views.py
+++ apps/affiliations/views.py
@@ -5,16 +5,16 @@ from .models import AffiliationPage, AffiliateProject
 def affiliation_list(request):
     data = {
         'affiliation_page': AffiliationPage.objects.first(),
-        'projects': AffiliateProject.objects.all().exclude(visibility=False)
+        'affiliated_projects': AffiliateProject.objects.all().exclude(visibility=False)
     }
     return render(request, 'affiliations/affiliation_list.html', data)
 
 
 def affiliation(request, project_id):
     data = {
-        # 'project_page': ProjectPage.objects.first(),
-        # 'project': Project.objects.get(pk=project_id),
-        # 'projects': Project.objects.all()
+        'affiliation_page': AffiliationPage.objects.first(),
+        'project': AffiliateProject.objects.get(pk=project_id),
+        'projects': AffiliateProject.objects.all().exclude(visibility=False)
     }
 
     # print(ProjectPage.objects.first(), Project.objects.get(pk=project_id))
